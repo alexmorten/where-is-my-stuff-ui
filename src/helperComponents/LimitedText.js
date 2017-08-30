50f897d2c446096370523f8ddb086c413b0fe23f
alexmorten <alexandrius.martinus@gmx.de>
2017-08-30 15:33:39 +0200

    made limitedText show-all-able

--- src/helperComponents/LimitedText.js
+++ src/helperComponents/LimitedText.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
 class LimitedText extends React.Component {
+  state={
+    open:false
+  }
+  handleOpen = (e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({open:true})
+  }
+  handleClose = (e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({open:false})
+  }
   render(){
     var {text,charLimit=100,...rest} = this.props;
 
@@ -16,9 +29,25 @@ class LimitedText extends React.Component {
         newText += " " +  wordArr[i] ;
       }
     }
-
+    var showMore;
+    var textItem;
+    if(!this.state.open){
+      showMore =  <span style={{color:"#5581ec",cursor:"pointer"}} onClick={this.handleOpen}> ... </span>
+      textItem = <span>{text.slice(0,newText.length)}</span>
+    }
+    var showLess;
+    if(this.state.open){
+      showLess = <span style={{color:"#5581ec",cursor:"pointer"}} onClick={this.handleClose}> show less <br/></span>
+      textItem = <span>{text}</span>
+    }
     return(
-      <span {...rest}>{text.slice(0,newText.length)} ...</span>
+      <span>
+        <span {...rest}>
+          {showLess}
+          {textItem}
+          {showMore}
+        </span>
+      </span>
     )
   }
 }
